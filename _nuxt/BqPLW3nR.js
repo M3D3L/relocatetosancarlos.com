8ddef24a8fd62b28924f18d036d0b24489005c2e
M3D3L <72050286+M3D3L@users.noreply.github.com>
2025-10-05 21:35:56 +0000

    Deploying to gh-pages from @ M3D3L/common@20e205a55073c7bcc418017683e3e967f582a84e 🚀

---
+++ _nuxt/BqPLW3nR.js
@@ -0,0 +1,1 @@
+import{e as n,c as o,o as t,r,D as c,u as l,x as i}from"#entry";const d=n({__name:"CardTitle",props:{class:{}},setup(s){const e=s;return(a,p)=>(t(),o("h3",{class:c(l(i)("font-semibold leading-none tracking-tight",e.class))},[r(a.$slots,"default")],2))}});export{d as _};
---
+++ _nuxt/CRzVOkCq.js
@@ -0,0 +1,11 @@
+import{y as h,z as C,h as v,c as g,C as k,o as m,a as r,b as i,w as d,u as t,g as f,H as V,I as w,d as N,t as q,D as z}from"#entry";import{c as x}from"./US0qkxtK.js";/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=x("chevron-left",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=x("chevron-right",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]),I={key:0,class:"flex items-center justify-center"},L={class:"flex items-center gap-1"},M={__name:"Pagination",props:{totalPages:{type:Number,required:!0},maxVisiblePages:{type:Number,default:5},showPagination:{type:Boolean,default:!1}},setup(u){const s=u,P=h(),y=C(),o=v(()=>parseInt(P.query.page)||1),b=v(()=>{const l=[];let e=1,a=s.totalPages;if(s.totalPages>s.maxVisiblePages){const n=Math.floor(s.maxVisiblePages/2),p=Math.ceil(s.maxVisiblePages/2)-1;o.value<=n?a=s.maxVisiblePages:o.value+p>=s.totalPages?e=s.totalPages-s.maxVisiblePages+1:(e=o.value-n,a=o.value+p)}for(let n=e;n<=a;n++)l.push(n);return l}),c=l=>{l<1||l>s.totalPages||y.push({query:{...P.query,page:l}})};return(l,e)=>u.showPagination&&u.totalPages>1?(m(),g("nav",I,[r("ul",L,[r("li",null,[i(t(f),{variant:"outline",size:"sm",disabled:t(o)===1,onClick:e[0]||(e[0]=a=>c(t(o)-1))},{default:d(()=>[i(t(B),{class:"w-4 h-4"}),e[2]||(e[2]=r("span",{class:"sr-only"},"Previous page",-1))]),_:1},8,["disabled"])]),(m(!0),g(V,null,w(t(b),a=>(m(),g("li",{key:a},[i(t(f),{variant:"page",class:z(["px-4",t(o)===a?"bg-primary text-primary-foreground":""]),size:"sm",onClick:n=>c(a)},{default:d(()=>[N(q(a),1)]),_:2},1032,["class","onClick"])]))),128)),r("li",null,[i(t(f),{variant:"outline",size:"sm",disabled:t(o)===u.totalPages,onClick:e[1]||(e[1]=a=>c(t(o)+1))},{default:d(()=>[i(t($),{class:"w-4 h-4"}),e[3]||(e[3]=r("span",{class:"sr-only"},"Next page",-1))]),_:1},8,["disabled"])])])])):k("",!0)}};export{M as _};
