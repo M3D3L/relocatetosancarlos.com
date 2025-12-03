96a66d4db2852509cd9f60bcde79c2daa87f6fa8
M3D3L <72050286+M3D3L@users.noreply.github.com>
2025-12-03 04:17:27 +0000

    Deploying to gh-pages from @ M3D3L/common@fdf6f5a306a14843398dbdde71db8efffce8b1cf 🚀

---
+++ _nuxt/Bh0D9h3B.js
@@ -0,0 +1,6 @@
+import{c as n}from"./DRrjGkPy.js";import{e as d,L as u,M as i,u as s,c as m,o as c,n as p,K as f}from"#entry";import{c as x}from"./DFb2OhTB.js";import{a as b}from"./D6GclQTL.js";/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=n("x",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]),y=d({__name:"Textarea",props:{class:{},defaultValue:{},modelValue:{}},emits:["update:modelValue"],setup(l,{emit:t}){const e=l,o=b(e,"modelValue",t,{defaultValue:e.defaultValue});return(v,a)=>u((c(),m("textarea",{"onUpdate:modelValue":a[0]||(a[0]=r=>f(o)?o.value=r:null),class:p(s(x)("flex min-h-[60px] w-full rounded-md border border-foreground/70 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",e.class))},null,2)),[[i,s(o)]])}});export{w as X,y as _};
